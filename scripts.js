7b30bfe31461bcfdb7377079ffe31419774fa092
Sai Vanga <Sai@Sai.Rocks>
2021-08-03 11:56:13 -0700

    Adding Delete Button

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
-let saved = document.getElementById("saved");
-let clear = document.getElementById("clear");
-let saveButton = document.getElementById("save-button");
-let savedDiv = document.getElementById("saved-div");
-let colorDiv = document.getElementById("random-color-box");
-let r = document.getElementById("range-red");
-let g = document.getElementById("range-green");
-let b = document.getElementById("range-blue");
+const saved = document.getElementById("saved");
+const clear = document.getElementById("clear");
+const saveButton = document.getElementById("save-button");
+const savedDiv = document.getElementById("saved-div");
+const colorDiv = document.getElementById("random-color-box");
+const r = document.getElementById("range-red");
+const g = document.getElementById("range-green");
+const b = document.getElementById("range-blue");
+const btned = document.createElement("button");
 
 function updateColor() {
   const red = r.value;
@@ -38,20 +39,27 @@ const renderColorsFromLocalStorage = () => {
   }
 
   // Make sure you empty existing colors.
-  // Get color string from local storage
-  // Convert string to array of objects
-  // Iterate on the array
-  /*
-    [{r: 1, g: 2, b: 10}, {r: 100, g: 20, b: 200}, ....]
-    for (let i = 0; i < colors.length; i++) {
-      const color = colors[i]; // {r: 1, g: 2, b: 10}
+  savedDiv.innerText = "";
 
-      // Now render this color
-        // Create a new div
-        // Set background color
-        // Append
-    }
-  */
+  // Get color string from local storage
+  const colorString2 = JSON.stringify(localStorage.getItem("colors"));
+
+  // Convert string to array of objects
+  const colorArray = JSON.parse(colorString2);
+
+  // Iterate on the array
+  for (let i = 0; i < colorsArray.length; i++) {
+    const color = colorsArray[i]; // {r: 1, g: 2, b: 10}
+
+    // Now render this color
+
+    // Create a new div
+    // Set background color
+    // Append
+  }
+  /*
+  [{r: 1, g: 2, b: 10}, {r: 100, g: 20, b: 200}, ....]
+      */
 };
 
 saveButton.addEventListener("click", function () {
@@ -80,3 +88,29 @@ clear.addEventListener("click", () => {
 
 // After first load, render existing colors
 renderColorsFromLocalStorage();
+
+const deleteColorFromLocalStorage = (index) => {
+  // Test if index is not null or undefined
+
+  if (index === null || index === undefined || index.trim() === null) {
+  } else {
+    // Get color string from local storage
+    const colorString = JSON.stringify(localStorage.getItem("colors"));
+
+    // Convert color string to an array
+    const colorArray = JSON.parse(colorString);
+
+    // Check if color array is null
+    if (colorArray === null) {
+      colorArray === null;
+    }
+
+    // delete color at index from color array
+
+    // convert color array to color string
+
+    // save color string in local storage
+
+    // Call render colors ui from local storage here
+  }
+};
